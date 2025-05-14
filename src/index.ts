78d4630a31ec78037b1b82b6070f975f14508e53
vivek-800 <vkrao800@gmail.com>
2025-05-14 09:35:12 +0530

    added try catch

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 require('dotenv').config();
-import  { Request, Response } from "express";
+import { Request, Response } from "express";
 import express from "express"
 import z, { string } from "zod";
 import { contentModel, linkModel, userModel } from "./db";
 import jwt from "jsonwebtoken";
 import { UserMiddleware } from "./middleware";
 import { hash } from "./util";
+import bcrypt from "bcrypt";
 
 const secret = process.env.JWT_SECRET;
 if (!secret) {
@@ -40,11 +41,12 @@ app.post("/api/v1/signup", async (req, res) => {
       return;
     }
     const { name, username, password } = parsedData.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     await userModel.create({
       name: name,
       username: username,
-      password: password,
+      password: hashedPassword,
     });
 
     res.status(200).json({
@@ -62,25 +64,21 @@ app.post("/api/v1/signin", async (req, res) => {
   try {
     const parsedData = SigninInput.safeParse(req.body);
     if (!parsedData.success) {
-      res.status(411).json({
-        message: "Error in inputs"
+      res.status(400).json({
+        message: ""
 
       })
       return;
     }
     const { username, password } = parsedData.data;
 
-    const response = await userModel.findOne({
-      username,
-      password
-    })
-    console.log(response);
-    if (!response) {
-      res.status(404).json({
-        message: "Not Signed up"
-      })
+    const response = await userModel.findOne({ username });
+    if (!response || !(await bcrypt.compare(password, response.password))) {
+      res.status(401).json({ message: "Invalid credentials" });
       return;
-    } else {
+    }
+
+    else {
       const token = jwt.sign({
         id: response._id
       }, secret)
@@ -101,91 +99,119 @@ app.post("/api/v1/signin", async (req, res) => {
 })
 
 app.post("/api/v1/content", UserMiddleware, async (req: Request, res: Response) => {
-  const { link, title, type } = req.body;
-  if (!link || !title || !type) {
-    res.status(404).json({
-      message: "contents fields are missing"
+  try {
+    const { link, title, type } = req.body;
+    if (!link || !title || !type) {
+      res.status(404).json({
+        message: "contents fields are missing"
+      })
+      return;
+    }
+
+    await contentModel.create({
+      link: link,
+      title: title,
+      type: type,
+      tags: [],
+      userId: req.userId
+    });
+
+    res.status(200).json({
+      message: "Content created successfully"
+    });
+  } catch (e) {
+    res.status(500).json({
+      message: "Internal Server Error"
     })
-    return;
   }
 
-  await contentModel.create({
-    link: link,
-    title: title,
-    type: type,
-    tags: [],
-    userId: req.userId
-  });
-
-  res.status(200).json({
-    message: "Content created successfully"
-  });
 });
 
 app.get("/api/v1/content", UserMiddleware, async (req, res) => {
+  try {
     const userId = req.userId;
-   const content = await contentModel.findOne({
+    const content = await contentModel.find({
       userId
     }).populate("userId", "username");
 
     res.status(200).json({
       content
     })
+  } catch (e) {
+    res.status(500).json({
+      message: "Internal Server Error"
+    })
+  }
+
 
 });
 
 app.delete("/api/v1/content", UserMiddleware, async (req, res) => {
-  const contentId = req.body.contentId;
- const response = await contentModel.deleteOne({
-    _id: contentId,
-    userId:req.userId
-  })
-  if(!response){
-    res.status(403).json({
-      message:"you are not the author of this content"
+  try {
+    const contentId = req.body.contentId;
+    const response = await contentModel.deleteOne({
+      _id: contentId,
+      userId: req.userId
     })
-    return;
+    if (!response) {
+      res.status(403).json({
+        message: "you are not the author of this content"
+      })
+      return;
+    }
+    res.status(200).json({
+      message: "content deleted",
+    });
+  } catch (e) {
+    res.status(500).json({
+      message: "Internal Server Error"
+    })
   }
-  res.status(200).json({
-    message: "content deleted",
-  });
+
 });
 
 app.post("/api/v1/brain/share", UserMiddleware, async (req, res) => {
-  const share = req.body.share;
-  if(share){
-    const shareLink = hash(10);
-    await linkModel.create({
-      hash: shareLink,
-      userId: req.userId
-    })
-    res.status(200).json({
-      message: "Share link created",
-      shareLink
-    })
-  }else{
-    await linkModel.deleteOne({
-      userId: req.userId
+  try {
+    const share = req.body.share;
+    if (share) {
+      const shareLink = hash(10);
+      await linkModel.create({
+        hash: shareLink,
+        userId: req.userId
+      })
+      res.status(200).json({
+        message: "Share link created",
+        shareLink
+      })
+    } else {
+      await linkModel.deleteOne({
+        userId: req.userId
 
+      })
+      res.status(201).json({
+        message: "ShareLink is disabled",
+      });
+    }
+  } catch (e) {
+    res.status(500).json({
+      message: "Internal Server Error"
     })
-    res.status(201).json({
-      message: "ShareLink is disabled",
-    });
   }
- 
+
+
 });
 
 app.get("/api/v1/brain/:shareLink", async (req, res) => {
   try {
     const shareLink = req.params.shareLink;
-    
+
     // Find the user associated with this share link
     const linkDoc = await linkModel.findOne({
       hash: shareLink
     });
 
     if (!linkDoc) {
-       res.status(404).json({
+      res.status(404).json({
         message: "Share link not found or expired"
       });
       return;
